bb244abbe75ea622d81af3ead74eb0b504116c19
Happy Soni <Happy.soni2002@gmail.com>
2024-11-23 16:24:21 +0530

    price

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -9,12 +9,13 @@ const Menu = () => {
                 <h1 className="heading">POPULAR DISHES</h1>
                 <p>Discover our most-loved dishes, crafted to perfection and packed with irresistible flavors.</p>
             </div>
-            <div className="dishes_container">
+            <div className="dishes_container" >
                 {
                     data[0].dishes.map(element => (
                         <div className="card" key={element.id}>
                                 <img src={element.image} alt={element.title} />
                                 <h3>{element.title}</h3>
+                                <p>{element.price}</p>
                                 <button>{element.category}</button>
                         </div>
                     ))
