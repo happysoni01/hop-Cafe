cf64d176c22f2f9934bceb5b3711a243873d4d1f
happysoni01 <166524499+happysoni01@users.noreply.github.com>
2024-11-23 17:17:41 +0530

    Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <>
       <nav>
-        <div className="logo"><img src="frontend/public/brand.png" alt="Hop Café" /></div>
+        <div className="logo"><img src="./brand.png" alt="Hop Café" /></div>
         <div className={show ? "navLinks showmenu" : "navLinks"}>
           <div className="links">
             {data[0].navbarLinks.map((element) => (
